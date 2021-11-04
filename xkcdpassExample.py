e51cbbe033879b3c4073f87b25467fe32b0ca550
Charles-DJM <43938497+Charles-DJM@users.noreply.github.com>
2021-11-04 11:49:32 +0100

    Initial commit

---
+++ xkcdpassExample.py
@@ -0,0 +1,9 @@
+from xkcdpass import xkcd_password as xp
+# taken from https://github.com/redacted/XKCD-password-generator
+# create a wordlist from the default wordfile
+# use words between 4 and 9 letters long
+wordfile = xp.locate_wordfile()
+mywords = xp.generate_wordlist(wordfile=wordfile, min_length=4, max_length=9)
+
+# create a password with the acrostic "face"
+print(xp.generate_xkcdpassword(mywords, acrostic="face"))
