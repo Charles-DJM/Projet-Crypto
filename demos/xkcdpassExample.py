7993e9621af93db3a83e1f78ebe0a4a7dcc9d05c
Charles-DJM <43938497+Charles-DJM@users.noreply.github.com>
2021-11-28 13:52:07 +0100

    Update xkcdpassExample.py

--- demos/xkcdpassExample.py
+++ demos/xkcdpassExample.py
@@ -2,7 +2,7 @@ from xkcdpass import xkcd_password as xp
 # taken from https://github.com/redacted/XKCD-password-generator
 # create a wordlist from the default wordfile
 # use words between 4 and 9 letters long
-def gen_xkcd();
+def gen_xkcd():
     wordfile = xp.locate_wordfile()
     mywords = xp.generate_wordlist(wordfile=wordfile, min_length=4, max_length=9)
 
